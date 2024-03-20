b69da6a2a7cd64017d314a56b6adac558e983a40
mtezxx <mkukucko@gmail.com>
2024-03-20 20:46:45 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import  {Header}  from './components/Header'; 
+import {Balance} from './components/Balance';
+import {IncomeExpenses} from './components/IncomeExpenses';
+import {TransactionList} from './components/TransactionList';
 import './App.css';
 
 function App() {
   return (
     <div >
-      Hello world
+      <Header />
+      <div className='balance'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList/>
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/components/TransactionList.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+export const TransactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul className='list'>
+        <li className='minus'>
+            Cash <span>-400</span><button className='delete-btn'>X</button>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default TransactionList
